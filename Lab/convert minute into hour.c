49494e8f346ab12b48ead73412f1d757e4408c4c
shivamsingh.github19 <shivamsingh03082007@gmail.com>
2025-08-23 13:34:44 +0530

    Create convert minute into hour.c

---
+++ Lab/convert minute into hour.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int minutes;
+    int hours;
+
+    printf("Enter minutes: ");
+    scanf("%d", &minutes);
+
+    hours = minutes / 60;
+
+    printf("Hours: %d\n", hours);
+
+    return 0;
+}
